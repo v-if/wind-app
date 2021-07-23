8318eda4a696d042b84ad7ae39c48cb621fa2953
Taylor <taylor@cntt.co.kr>
2021-07-23 09:22:07 +0900

    Firebase Admob set android

--- components/Home.js
+++ components/Home.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { TouchableOpacity, Text, View, Image, StyleSheet, Platform } from 'react-native';
+import { TouchableOpacity, Text, View, Image, StyleSheet, Platform, Dimensions } from 'react-native';
 import Colors from './Colors';
 import { FlatGrid } from 'react-native-super-grid';
 import { WebView } from 'react-native-webview';
+import { BannerAd, BannerAdSize, TestIds } from '@react-native-firebase/admob';
 
 export default function Home({ navigation }) {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   const [width, setWidth] = useState(10);
   const [height, setHeight] = useState(10);
+
+  const win = Dimensions.get('window');
 
   function dataProcessing(res) {
     if(Platform.OS === 'ios') {
@@ -34,64 +37,85 @@ export default function Home({ navigation }) {
   }, []);
 
   return (
+  <View style={styles.container}>
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: Colors.white }}>
     {isLoading ? <Image style={{width: 300, height: 300}} source={require('./images/loading.gif')} /> : (
       <FlatGrid
-      itemDimension={130}
-      data={data}
-      style={styles.gridView}
-      // staticDimension={300}
-      // fixed
-      spacing={10}
-      renderItem={({ item }) => (
-        item.roadNm == 'ads_cpng' ? 
-        <View style={[styles.itemContainer]}>
-          <WebView
-              onLayout={(event) => {
-                var {x, y, width, height} = event.nativeEvent.layout;
-                setWidth(width * (Platform.OS === 'ios' ? 4 : 0.9))
-                setHeight(height * (Platform.OS === 'ios' ? 4 : 0.9))
-              }}
-              style={styles.adsWebView}
-              scalesPageToFit={false}
-              bounces={false}
-              scrollEnabled={false}
-              source={{html: '<iframe src="https://ads-partners.coupang.com/widgets.html?id='+item.imageFileName+'&template=carousel&trackingCode=AF4453206&subId=&width='+width+'&height='+height+'" width="'+width+'" height="'+height+'" frameborder="0" scrolling="no" referrerpolicy="unsafe-url"></iframe>'}}
-            />
-        </View> : 
-        <TouchableOpacity 
-          style={[styles.itemContainer]}
-          onPress={() => navigation.navigate('Map', {
-            road: item.road,
-            roadNm: item.roadNm,
-            latitude: item.latitude,
-            longitude: item.longitude,
-          })}
-        >
-          <View style={styles.item}>
-            <Image
-              style={styles.preview}
-              source={{ uri: "http://118.67.129.162/images/" + item.road }}
-            />
-            <Text style={styles.itemName}>
-              {item.roadNm.split('/').length > 1 ? item.roadNm.split('/')[0] : item.roadNm}
-            </Text>
-            <Text style={styles.itemName}>
-              {item.roadNm.split('/').length > 1 ? item.roadNm.split('/')[1] : ''}
-            </Text>
-          </View>
-        </TouchableOpacity>
+        itemDimension={130}
+        data={data}
+        style={{ width:win.width, flex: 1, backgroundColor: Colors.white, marginBottom: 50 }}
+        // staticDimension={300}
+        // fixed
+        spacing={10}
+        renderItem={({ item }) => (
+          item.roadNm == 'ads_cpng' ? 
+          <View style={[styles.itemContainer]}>
+            <WebView
+                onLayout={(event) => {
+                  var {x, y, width, height} = event.nativeEvent.layout;
+                  setWidth(width * (Platform.OS === 'ios' ? 4 : 0.9))
+                  setHeight(height * (Platform.OS === 'ios' ? 4 : 0.9))
+                }}
+                style={styles.adsWebView}
+                scalesPageToFit={false}
+                bounces={false}
+                scrollEnabled={false}
+                source={{html: '<iframe src="https://ads-partners.coupang.com/widgets.html?id='+item.imageFileName+'&template=carousel&trackingCode=AF4453206&subId=&width='+width+'&height='+height+'" width="'+width+'" height="'+height+'" frameborder="0" scrolling="no" referrerpolicy="unsafe-url"></iframe>'}}
+              />
+          </View> : 
+          <TouchableOpacity 
+            style={[styles.itemContainer]}
+            onPress={() => navigation.navigate('Map', {
+              road: item.road,
+              roadNm: item.roadNm,
+              latitude: item.latitude,
+              longitude: item.longitude,
+            })}
+          >
+            <View style={styles.item}>
+              <Image
+                style={styles.preview}
+                source={{ uri: "http://118.67.129.162/images/" + item.road }}
+              />
+              <Text style={styles.itemName}>
+                {item.roadNm.split('/').length > 1 ? item.roadNm.split('/')[0] : item.roadNm}
+              </Text>
+              <Text style={styles.itemName}>
+                {item.roadNm.split('/').length > 1 ? item.roadNm.split('/')[1] : ''}
+              </Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
       )}
-    />
-    )}
+    </View>
+    <View style={styles.admob}>
+      <BannerAd
+        unitId={TestIds.BANNER}
+        size={BannerAdSize.FULL_BANNER}
+        requestOptions={{
+          requestNonPersonalizedAdsOnly: true,
+        }}
+        onAdLoaded={function() {
+          console.log('Advert loaded');
+        }}
+        onAdFailedToLoad={function(error) {
+          console.error('Advert failed to load: ', error);
+        }}
+      />
+    </View>
   </View>
   );
 }
 
 const styles = StyleSheet.create({
-  gridView: {
+  container: {
     flex: 1,
-    backgroundColor: Colors.white,
+  },
+  admob: {
+    position: 'absolute',
+    bottom: 0,
+    backgroundColor: Colors.white
   },
   adsWebView: {
     flex: 1,
